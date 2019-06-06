9d67ebd21f17db80865b8a525d986751f718b092
adsonb87 <adsonb87@gmail.com>
2019-06-05 21:14:10 -0300

    Rotas Professor

---
+++ app/models/ProfessorDAO.js
@@ -0,0 +1,27 @@
+function ProfessorDAO(connection) {
+	this._connection = connection;
+}
+
+ProfessorDAO.prototype.listaProfessores = function(callback) {
+	this._connection.query('select * from professor', callback);
+}
+
+ProfessorDAO.prototype.setProfessor = function(professor, callback){
+	this._connection.query('insert into professor set ?',professor,callback);
+}
+
+ProfessorDAO.prototype.getProfessor = function(matricula, callback){
+	this._connection.query('select * from professor where ?', matricula, callback);
+}
+
+ProfessorDAO.prototype.deleteProfessor = function(matricula, callback){
+	this._connection.query('delete from professor where ?', matricula, callback);
+}
+
+ProfessorDAO.prototype.updateProfessor = function(professor, callback){
+	this._connection.query('update professor set ?', professor, callback);
+}
+
+module.exports = function(){
+	return ProfessorDAO;
+}
